0dd0d37e3e8880dea94e1e676c2f2b1645ffb222
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-05-08 12:36:47 -0400

    Fix dependencies for RacketCon addition.

--- info.rkt
+++ info.rkt
@@ -9,10 +9,12 @@
                "racket-index"
                "scribble-lib"
                "syntax-color-lib"
+               "plot-gui-lib"
+               "math-lib"
                "pollen")) ; for rcon
 
 (define pkg-desc "Sources for http://racket-lang.org")
 
-(define pkg-authors '(eli mflatt samth))
+(define pkg-authors '(eli mflatt samth stamourv))
 
 (define test-omit-paths '("sync.rkt"))
