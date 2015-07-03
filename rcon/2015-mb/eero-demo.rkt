777752168be7c20d9334537825b1c74a904d3924
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-03 11:31:19 -0500

    Rackety code example.

--- rcon/2015-mb/eero-demo.rkt
+++ rcon/2015-mb/eero-demo.rkt
@@ -1,22 +1,21 @@
 #lang racket
-(require math plot file/gzip)
+(require math plot)
 ;; variations on the Gateway Arch
 ;; see https://en.wikipedia.org/wiki/Gateway_Arch#Mathematical_elements
-(let* ([fc 625.0925]
-       [Qb 1262.6651]
-       [Qt 125.1406]
-       [L 299.2239]
-       [A (/ fc (sub1 (/ Qb Qt)))]
-       [C (acosh (/ Qb Qt))]
-       [eero (λ(x) (* -1 A (sub1 (cosh (/ (* C x) L)))))]
-       [normalized-eero (λ(w) (inverse (λ(x) (/ (eero (* w x)) w)) -1 1))]
-       [hsize 800] [vsize 400] [spread 700])
-  (plot-decorations? #f)
-  (line-samples 100)
-  (define op (open-output-string))
-  (plot (map (λ(x)
-               (parameterize ([line-color (inexact->exact (floor (* 128 (random))))]
-                              [line-width (random)])
-                 (normalized-eero x))) (range (* -1 spread) spread 6))
+(define fc 625.0925)
+(define Qb 1262.6651)
+(define Qt 125.1406)
+(define L 299.2239)
+(define A (/ fc (sub1 (/ Qb Qt))))
+(define C (acosh (/ Qb Qt)))
+(define (eero x) (* -1 A (sub1 (cosh (/ (* C x) L)))))
+(define (normalized-eero w) (inverse (λ (x) (/ (eero (* w x)) w)) -1 1))
+(define spread 700)
+(parameterize ([plot-decorations? #f]
+               [line-samples 100])
+  (plot (for/list ([x (in-range (* -1 spread) spread 6)])
+          (parameterize ([line-color (random 128)]
+                         [line-width (random)])
+            (normalized-eero x)))
         #:x-min -1 #:x-max 1 #:y-min -1 #:y-max 1
-        #:width hsize #:height vsize))
+        #:width 800 #:height 400))
