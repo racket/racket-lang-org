61a4505a3d6d45c15329e20e964e0fde5786091f
Vincent St-Amour <stamourv@racket-lang.org>
2015-09-27 09:21:20 -0500

    Pointer to live stream.

--- rcon/2015-mb/index.html.pm
+++ rcon/2015-mb/index.html.pm
@@ -14,6 +14,9 @@
 
 RacketCon is a public meeting for everyone interested in Racket: developers, contributors, programmers, educators, and bystanders. It's an opportunity for all members of the community to come together to share plans, ideas, and enthusiasm. RacketCon will enable the entire Racket community to mingle: to update each other, to exchange ideas, to collaborate, and to help shape the future of Racket.
 
+◊h3{◊xtarget["stream"]{Live Stream}}
+◊link["https://t.co/6n2mEHIl9c"]{Via Google Hangouts.}
+
 ◊h3{◊xtarget["register"]{Register}}
 ◊link["https://eventbrite.com/event/16825218682/"]{Via Eventbrite.}
 Use the "RAC" group code when reserving at the ◊link["http://doubletree3.hilton.com/en/hotels/missouri/st-louis-union-station-a-doubletree-by-hilton-hotel-STLUSDT/index.html"]{Union Station hotel}.
