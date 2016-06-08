69465824f97145735598e29f3491b9f81d23541a
Vincent St-Amour <stamourv@racket-lang.org>
2016-06-07 20:36:13 -0500

    Update upload script for new 2016 page.

--- rcon/all.rkt
+++ rcon/all.rkt
@@ -26,19 +26,29 @@
 ;; TODO build the pollen file (and the SVGs) when the site is built, and remove
 ;;  the generated files from the repo
 
+;; copy over 2015 site
 (define-runtime-path 2015-dir "2015-mb")
-
-(for ([f (in-list '("eero.svg" "eero.svgz" "cubit.png" "pattern.png" "styles.css"))])
+(for ([f (in-list '("eero.svg" "eero.svgz" "cubit.png" "pattern.png"
+                    "styles.css" "index.html"))])
   (void (copyfile #:site con-site
-                  (build-path 2015-dir f) (string-append "2015/" f)))
-  (void (copyfile #:site con-site ; root of con-site, for current year
-                  (build-path 2015-dir f))))
-
-(void (copyfile #:site con-site
-                (build-path 2015-dir "index.html") "2015/index.html"))
-
-(define-runtime-path fonts "2015-mb/fonts/")
-(for ([f (in-directory fonts)])
+                  (build-path 2015-dir f) (string-append "2015/" f))))
+(define-runtime-path 2015-fonts "2015-mb/fonts/")
+(for ([f (in-directory 2015-fonts)])
   (define-values (base name _) (split-path f))
-  (copyfile #:site con-site f (string-append "fonts/" (path->string name)))
   (copyfile #:site con-site f (string-append "2015/fonts/" (path->string name))))
+
+(define-runtime-path 2016-dir "2016")
+(for ([f (in-list (directory-list 2016-dir))])
+  (define-values (base name _) (split-path f))
+  (define s (path->string name))
+  (when (or (and (regexp-match? "html$" s)
+                 (not (equal? s "index.html"))) ; copied above
+            (regexp-match? "css$" s)
+            (regexp-match? "svg$" s)
+            (regexp-match? "png$" s))
+    (void (copyfile #:site con-site
+                    (build-path 2016-dir f)))))
+(define-runtime-path 2016-fonts "2016/fonts/")
+(for ([f (in-directory 2016-fonts)])
+  (define-values (base name _) (split-path f))
+  (copyfile #:site con-site f (string-append "fonts/" (path->string name))))
