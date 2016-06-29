e40a5e4864d29d627f94c9b5b30fc3af0c260dce
Vincent St-Amour <stamourv@racket-lang.org>
2016-06-29 15:31:04 -0500

    Info for Stephen and Alex.

--- rcon/2016/index.html.pm
+++ rcon/2016/index.html.pm
@@ -43,6 +43,17 @@ I've been devoting the year 2016 strictly to Racket projects. The most ambitious
 
 
 ◊folded{
+◊speaker["" "Stephen Chang and Alex Knauth"]{Type Systems as Macros}
+◊;rlink["2015/mccarthy.pdf"]{Slides} ◊;link["https://www.youtube.com/watch?v=BDg79CPbdXQ&list=PLXr4KViVC0qJAsNuDeQzhFDjMK1gEdls8&index=2"]{Video}
+
+The Racket Manifesto says that "Racket is a programming language for creating new programming languages". Yet, nearly all languages created with Racket are untyped, ignoring the significant typed portion of the PL spectrum. Typed Racket demonstrates that creating typed languages in Racket is possible ... with monumental effort. This talk attempts to give hope to the common Racket programmer who wishes to create typed languages with Racket, yet does not possess multiple PhDs. Specifically, I'll demonstrate a technique for embedding type checking directly into macro definitions, in a manner that is compatible with the language-creation patterns that Racket programmers are already familiar with, thus leading to typed languages with little effort.
+
+◊bio{◊link["http://www.ccs.neu.edu/home/stchang/"]{Stephen Chang} is a research scientist at Northeastern University studying programming language design. He works extensively with Racket and enjoys looking for novel applications of its macro system.
+
+◊link["https://github.com/AlexKnauth"]{Alex Knauth} is a computer science and physics major at Northeastern University who loves learning. In pursuit of that goal, he likes working with Racket and Typed Racket, and loves discovering new ways of using Racket macros.}}
+
+
+◊folded{
 ◊speaker["" "Byron Davies"]{Racket Does Dijkstra}
 ◊;rlink["2015/mccarthy.pdf"]{Slides} ◊;link["https://www.youtube.com/watch?v=BDg79CPbdXQ&list=PLXr4KViVC0qJAsNuDeQzhFDjMK1gEdls8&index=2"]{Video}
 
@@ -98,7 +109,6 @@ I’ve been working on ◊link["https://github.com/brucehs/simple-hue-control-ra
 ◊h3{Plus}
 
 ◊div[#:class "two-col"]{
-◊speaker["" ◊link["http://www.ccs.neu.edu/home/stchang/"]{Stephen Chang} ""]{}
 ◊speaker["" "Linh Chi Nguyen" ""]{}
 ◊speaker["" "(more speakers to come)" ""]{}
 }
