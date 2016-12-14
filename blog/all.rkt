d25a4ec3a9f9be2f376caa227b286b74a58d39a4
Matthew Butterick <mb@mbtype.com>
2016-12-14 12:12:41 -0800

    add `frog` and `pollen` to build script

--- blog/all.rkt
+++ blog/all.rkt
@@ -1,6 +1,7 @@
 #lang plt-web
 (require plt-web/style
          racket/dict racket/match racket/runtime-path racket/path
+         racket/system compiler/find-exe
          "../identity.rkt"
          "../testing.rkt")
 
@@ -8,6 +9,13 @@
   (site "blog"
         #:url (rewrite-for-testing "https://blog.racket-lang.org/")))
 (register-identity blog-site)
+
+(define-runtime-path blog-dir ".")
+(define css-dir (simplify-path (build-path blog-dir "css/")))
+(define raco-path (let-values ([(base name _) (split-path (find-exe))])
+                    (build-path base "raco")))
+(system (format "~a frog -b" raco-path)) ; frog rebuild: generates blog html
+(system (format "~a pollen render ~a" raco-path css-dir)) ; pollen rebuild: generates css
 
 (define (excluded-path? path)
   (define-values (base name _) (split-path path))
@@ -19,7 +27,6 @@
    ;; racket source
    (regexp-match #rx"\\.rkt$" (path->string name))))
 
-(define-runtime-path blog-dir ".")
 (for ([path (in-directory blog-dir)]
       #:unless (or (directory-exists? path)
                    (excluded-path? path)))
