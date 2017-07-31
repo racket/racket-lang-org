257b4d8fdae3c8b081ab2a70a412801702ba6d98
Matthew Flatt <mflatt@racket-lang.org>
2017-07-31 16:57:07 -0600

    restore HTML documentation redirects
    It turns out that the release build refers to documentation
    (for indirect links) at the old URL, so we need to keep those
    links. The PDF ones are still gone.

--- download/symlinks.rkt
+++ download/symlinks.rkt
@@ -11,25 +11,33 @@
 (define (new-version? v)
   (version<=? "5.92" v))
 
-;; We used to map the old layout to new layout, but that mapping is
-;; now disabled, because we ran out of S3 redirects.
-(when #f
-  (define (version-symlink src-v dest-v)
-    (symlink #:site download-site
-             (format "../../releases/~a/doc" dest-v)
-             (format "docs/~a/html" src-v))
+(define (need-html-redirect-version? v)
+  (and (new-version? v)
+       (version<=? v "6.10")))
+
+;; Map the old layout to new layout, at least for HTML up to v6.10,
+;; because those URLs are used in documentation redirects. The
+;; distribution after v6.10 doesn't rely on the redirects, so
+;; we leave out the symlinks to conserve S3 redirects.
+(define (version-symlink src-v dest-v)
+  (symlink #:site download-site
+           (format "../../releases/~a/doc" dest-v)
+           (format "docs/~a/html" src-v))
+  (when #f
     (symlink #:site download-site
              (format "../../releases/~a/pdf-doc" dest-v)
-             (format "docs/~a/pdf" src-v)))
+             (format "docs/~a/pdf" src-v))))
 
-  (for ([r (in-list all-releases)])
-    (define v (release-version r))
-    (when (new-version? v)
-      (version-symlink v v)))
+(for ([r (in-list all-releases)])
+  (define v (release-version r))
+  (when (need-html-redirect-version? v)
+    (version-symlink v v)))
 
-  ;; Add "recent" links for the latest version:
-  (unless (null? all-releases)
-    (version-symlink "recent" (release-version (car all-releases)))))
+;; We used to add "recent" links for the latest version, but
+;; this is now disabled
+#;
+(unless (null? all-releases)
+  (version-symlink "recent" (release-version (car all-releases))))
 
 ;; We generally expect "index.html" files to be in place in "releases"
 ;; and for pre-v6.0 "docs", but keep "installers", "docs", and
