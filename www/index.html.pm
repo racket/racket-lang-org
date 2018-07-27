7030b51a121e06d59c5262ce827975128714c567
Vincent St-Amour <stamourv@racket-lang.org>
2018-07-27 14:09:03 -0500

    v7.0 blog post

--- www/index.html.pm
+++ www/index.html.pm
@@ -169,7 +169,7 @@ The ◊code{web-server/insta} language lets you quickly prototype server-side we
 ◊section{
 Software
 
-◊link["https://download.racket-lang.org/"]{Download Racket v6.12}
+◊link["https://download.racket-lang.org/"]{Download Racket v7.0}
 
 ◊link["https://github.com/plt/racket/"]{Source code}
 
@@ -204,7 +204,7 @@ Documentation & tutorials
 ◊section{
 News
 
-◊link["https://blog.racket-lang.org/2018/01/racket-v6-12.html"]{Racket version 6.12} is available.
+◊link["https://blog.racket-lang.org/2018/07/racket-v7-0.html"]{Racket version 7.0} is available.
 
 ◊link["https://con.racket-lang.org/"]{(eighth RacketCon)} will be held in St. Louis, 29–30 Sept 2018.
 
