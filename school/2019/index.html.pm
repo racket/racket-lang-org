49d39062437842fe128fd15b153ce7a5306e0fc7
Matthias Felleisen <matthias@ccs.neu.edu>
2019-02-08 10:12:04 -0500

    language improvement

--- school/2019/index.html.pm
+++ school/2019/index.html.pm
@@ -23,7 +23,7 @@
 
 ◊h3{◊xtarget["overview"]{Overview}}
 
-◊link["http://racket-lang.org/" ◊img[#:style "float:left;width:5rem;padding-right:0.5rem" #:src "http://racket-lang.org/img/racket-logo.svg"]] The ◊link["http://racket-lang.org/"]{Racket} team has spent over thirty years developing and refining for creating domain-specific languages (DSLs). Racket School will introduce you to these tools and the underlying philosophy on language-oriented programming (LOP), ◊link[cacm]{cover article in the ◊strong{an approach to software development through the lenses of DSLs} (CACM, Feb 2018).
+◊link["http://racket-lang.org/" ◊img[#:style "float:left;width:5rem;padding-right:0.5rem" #:src "http://racket-lang.org/img/racket-logo.svg"]] The ◊link["http://racket-lang.org/"]{Racket} team has spent over thirty years developing and refining for creating domain-specific languages (DSLs). Racket School will introduce you to these tools and the underlying philosophy on language-oriented programming (LOP), ◊link[cacm]{an approach to software development through the lenses of DSLs} (CACM, Feb 2018).
 
 This year, Racket School is divided into two tracks, using two different pedagogical approaches. 
 
