26c4f146a35f2f48203080721af428149da0d74f
Matthias Felleisen <matthias@ccs.neu.edu>
2019-03-08 17:17:47 -0500

    added Picturing Programs

--- www/books.html.pm
+++ www/books.html.pm
@@ -17,47 +17,38 @@
 Books
 
 ◊link["https://www.htdp.org/"]{How to Design Programs}
-A principled approach to programming.
+Learn to design programs systematically.
 
+◊link["http://picturingprograms.com"]{Picturing Programs}
+Learn to design programs systematically, with pictures.
 
 ◊link["https://www.realmofracket.com/"]{Realm of Racket}
 Learn to program with Racket, one game at a time.
 
+◊link["https://gustavus.edu/mcs/max/concrete-abstractions.html"]{Concrete Abstractions}
+An introduction to computer science using Scheme.
 
 ◊link["https://beautifulracket.com/"]{Beautiful Racket}
 Make your own programming languages with Racket.
 
-
 ◊link["http://serverracket.com"]{Server: Racket}
 Develop a web application with Racket.
 
-
 ◊link["https://cs.brown.edu/~sk/Publications/Books/ProgLangs/2007-04-26/"]{Programming Languages: 
 Application and Interpretation}
-For undergraduates, graduate students, and experts.
+Principles of programming languages for undergraduates, graduate students, and experts.
 
-
-◊link["https://redex.racket-lang.org/"]{Semantics Engineering 
-with PLT Redex}
-Semantics automation for language engineers.
-
-
-◊link["https://gustavus.edu/mcs/max/concrete-abstractions.html"]{Concrete Abstractions}
-An introduction to computer science using Scheme.
-
+◊link["https://redex.racket-lang.org/"]{Semantics Engineering with PLT Redex}
+Semantics for language engineers: graduate students and practitioners alike. 
 
 ◊link["http://www.springer.com/de/book/9783835101555"]{Die Macht der Abstraktion}
 Einführung in die Programmierung.
 
-
-◊link["http://www.editions-ellipses.fr/product_info.php?products_id=7515"]{Premiers Cours de Programmation avec Scheme}
-Du fonctionnel pur aux objets avec DrRacket.
-
-
 ◊link["http://www.springer.com/us/book/9783658141332"]{Programmierparadigmen (2. Auflage)}
 Eine Einführung auf der Grundlage von Racket.
 
-
+◊link["http://www.editions-ellipses.fr/product_info.php?products_id=7515"]{Premiers Cours de Programmation avec Scheme}
+Du fonctionnel pur aux objets avec DrRacket.
 }
 
 
