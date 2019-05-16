f13f8d7baf2a53ff126bdb01a038014e07bcd4ac
Matthew Butterick <mb@mbtype.com>
2019-05-16 07:34:34 -0700

    home page banner for Racket Week

--- www/index.html.pm
+++ www/index.html.pm
@@ -15,6 +15,10 @@
 
 }
 
+◊special-section[#:class "one-column-body-text" #:id "pull-quote"]{Learn how to make programming languages at ◊link["https://school.racket-lang.org"]{Racket School 2019}, July 8–12 in Salt Lake City. Members of the ◊link["team.html"]{Racket team} will teach two multi-day workshops. Financial aid available for qualifying students. Followed by ◊link["https://con.racket-lang.org/"]{ninth RacketCon}. Join us!
+}
+
+
 ◊special-section{
 
 ◊feature["Batteries included" #:id "f1"]{Racket's extensive standard library gets your projects off the ground quickly.
