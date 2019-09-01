38ac3647ac5ce8b0610252e4e87a9eb0ceb82114
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2019-09-01 17:15:41 +0100

    Update friendly.html.pm
    not finished...

--- www/friendly.html.pm
+++ www/friendly.html.pm
@@ -20,10 +20,10 @@ This spirit requires an environment that enables all to participate without fear
 ◊section{
 Friendly Environment Policy
 
-* We are committed to providing a friendly, safe and welcoming environment for all, regardless of level of experience, gender identity and expression, sexual orientation, disability, personal appearance, body size, race, ethnicity, age, religion, nationality, or other similar characteristic.
+* We are committed to providing a friendly, safe and welcoming environment for all, regardless of level of experience or personal characteristics.
 * Please avoid using overtly sexual aliases or other nicknames that might detract from a friendly, safe and welcoming environment for all.
 * Please be kind and courteous. There’s no need to be mean or rude.
-* Respect that people have differences of opinion and that every design or implementation choice carries a trade-off and numerous costs. There is seldom a right answer.
+* Respect that people have differences of opinion and that every design or implementation choice carries a trade-off and numerous costs.
 * Please keep unstructured critique to a minimum. If you have solid ideas you want to experiment with, make a fork and see how it works.
 * We will exclude you from interaction if you insult, demean or harass anyone. That is not welcome behavior. We interpret the term “harassment” as including the definition in the Citizen Code of Conduct; if you have any lack of clarity about what might be included in that concept, please read their definition. In particular, we don’t tolerate behavior that excludes people in socially marginalized groups.
 * Private harassment is also unacceptable. No matter who you are, if you feel you have been or are being harassed or made uncomfortable by a community member, please contact one of the channel ops or any of the Rust moderation team immediately. Whether you’re a regular contributor or a newcomer, we care about making this community a safe place for you and we’ve got your back.
@@ -32,15 +32,13 @@ Friendly Environment Policy
 Anyone witnessing or subject to unacceptable behavior should ◊link["mailto:modration-team@racket-lang.org"]{EMAIL THE MODERATION TEAM}.
 
 If anynone engages in harassing behavior, the MODERATION TEAM may take any action they deem appropriate, from a warning of the offender to an expulsion.
-
-[The wording of this policy is directly derived from ◊link["http://snapl.org/2015/policy.html"]{that of the SNAPL conference}, with thanks.]
 } 
 
 ◊section{Moderation
 
 These are the policies for upholding our community’s standards of conduct. If you feel that a thread needs moderation, please contact the Rust moderation team .
 
-1. Remarks that violate the Racket standards of conduct, including hateful, hurtful, oppressive, or exclusionary remarks, are not allowed. (Cursing is allowed, but never targeting another user, and never in a hateful manner.)
+1. Remarks that violate the Racket standards of conduct, including hateful, hurtful, oppressive, or exclusionary remarks, are not allowed. Cursing is allowed, but never targeting another user, and never in a hateful manner.
 
 2. Remarks that moderators find inappropriate, whether listed in the code of conduct or not, are also not allowed.
 
@@ -50,7 +48,7 @@ These are the policies for upholding our community’s standards of conduct. If 
 
 5. If the user comes back and continues to make trouble, they will be banned, i.e., indefinitely excluded.
 
-6. Moderators may choose at their discretion to un-ban the user if it was a first offense and they offer the offended party a genuine apology.
+6. Moderators may choose at their discretion to un-ban the user if it was a first offense and the user offers the offended party a genuine apology.
 
 7. If a moderator bans someone and you think it was unjustified, please take it up with that moderator, or with a different moderator, in private. Complaints about bans in-channel are not allowed.
 
@@ -61,10 +59,6 @@ In the Racket community we strive to go the extra step to look out for each othe
 And if someone takes issue with something you said or did, resist the urge to be defensive. Just stop doing what it was they complained about and apologize. Even if you feel you were misinterpreted or unfairly accused, chances are good there was something you could’ve communicated better — remember that it’s your responsibility to make your fellow Racketeers comfortable. Everyone wants to get along and we are all here first and foremost because we want to talk about cool technology. You will find that people will be eager to assume good intent and forgive as long as you earn their trust.
 
 The enforcement policies listed above apply to all official Racket venues; including the Racket Slack channel; 
-#racket IRC on freenode.net; GitHub repositories under github.com/racket; and all forums under lists.racket-lang.org (including https://groups.google.com/forum/#!forum/racket-users). 
-
-For other projects adopting the Rust Code of Conduct, please contact the maintainers of those projects for enforcement. If you wish to use this code of conduct for your own project, consider explicitly mentioning your moderation policy or making a copy with your own moderation policy so as to avoid confusion.
-
-Adapted from the RacketCon 2018 Friendly Environment Policy and the ◊link["https://www.rust-lang.org/policies/code-of-conduct"]{Rust Code of conduct} which in turn was adapted from the Node.js Policy on Trolling as well as the Contributor Covenant v1.3.0.
+#racket IRC on freenode.net; GitHub repositories under github.com/racket; and all forums under lists.racket-lang.org (including https://groups.google.com/forum/#!forum/racket-users and https://groups.google.com/forum/#!forum/racket-dev). 
 
 }
