13522c373f84c6b7554e6d5a488504c000bfce61
Matthew Flatt <mflatt@racket-lang.org>
2020-04-30 09:24:40 -0600

    fix typo

--- www/friendly.html.pm
+++ www/friendly.html.pm
@@ -18,7 +18,7 @@ Friendly Environment Policy
 
 The Racket community aims to improve the world through programming. It started with the goal of introducing everyone to the wonderful world of program design, with a spirit of full inclusion and no emphasis on any specific group.  Over time it has grown into a full-fledged professional community with a well-known reputation for helpfulness and openness on its online communication channels.
 
-Racket project management wants to encourage and preserve this open exchange of ideas, which requires an environment that enables all to participate without fear of personal harassment. We define ◊em{harassment} to include specific unacceptable factors and behaviors listed in ◊link["https://www.acm.org/about-acm/policy-against-harassment"]{the ACM's policy aganist harassment}. Unacceptable behavior will not be tolerated.
+Racket project management wants to encourage and preserve this open exchange of ideas, which requires an environment that enables all to participate without fear of personal harassment. We define ◊em{harassment} to include specific unacceptable factors and behaviors listed in ◊link["https://www.acm.org/about-acm/policy-against-harassment"]{the ACM's policy against harassment}. Unacceptable behavior will not be tolerated.
 
 Anyone witnessing or subject to unacceptable behavior should notify ◊link["team.html#management"]{Racket project management}, which can be reached at ◊link["mailto:management@racket-lang.org"]{management@racket-lang.org}.
 
