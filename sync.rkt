5c2ecbefc1ac175fcc049b5c0fda53255ad19e1d
Alexander McLin <alex.mclin@gmail.com>
2020-08-18 15:44:23 -0400

    [sync.rkt] fixes so command-line options have intended behavior

--- sync.rkt
+++ sync.rkt
@@ -128,25 +128,26 @@
 
 (current-directory orig-dir)
 
-(cond
-  [(not render-locally?) (printf "Files saved in ~a\n" tmp-dir)]
-  [else 
+(if render-locally?
+  (let ([tmp-src (build-path tmp-dir "generated")]
+        [local-dest render-locally?])
     ;; move rendered pages into place 
     (when (directory-exists? render-locally?)
       (delete-directory/files render-locally?))
-    (define tmp-src (build-path tmp-dir "generated"))
-    (define local-dest render-locally?)
-    (copy-directory/files tmp-src local-dest #:preserve-links? #t)
-    (when (not save-temps?) (delete-directory/files tmp-dir))
+    (copy-directory/files tmp-src local-dest #:preserve-links? #t))
+  (printf "Files saved in ~a\n" tmp-dir))
 
-    ;; generate and move code into place
-    #;
-    (begin
-    (parameterize ((current-directory "www/img/"))
-      (system "./general-purpose-x"))
-    (define gp "general-purpose/")
-    (define gp-src (string-append "www/img/" gp))
-    (define dest (string-append render-locally? "/www/" gp))
-    (rename-file-or-directory gp-src dest #t))])
+(unless save-temps?
+  (delete-directory/files tmp-dir))
+
+;; generate and move code into place
+#;
+(begin
+ (parameterize ((current-directory "www/img/"))
+ (system "./general-purpose-x"))
+ (define gp "general-purpose/")
+ (define gp-src (string-append "www/img/" gp))
+ (define dest (string-append render-locally? "/www/" gp))
+ (rename-file-or-directory gp-src dest #t))
 
 (printf "\n\nIf you updated any CSS file, please purge it from the Cloudflare cache.\n\n")
