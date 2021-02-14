7b997f79384687b827b74f21d550e79894add098
John Clements <clements@racket-lang.org>
2021-02-13 17:47:40 -0800

    v8.0 blog post and front page

--- www/index.html.pm
+++ www/index.html.pm
@@ -41,7 +41,7 @@
 ◊special-section[#:class "one-column-body-text" #:style "font-size:77%" #:id "pull-quote"]{
 ◊div[#:class "container-fluid vertical-in-smartphones-horizontal-otherwise"]{
  ◊div[#:style "margin-left: 1em"]{
-   ◊link["https://blog.racket-lang.org/2020/11/racket-v7-9.html"]{Racket version 7.9} is available.}
+   ◊link["https://blog.racket-lang.org/2021/02/racket-v8-0.html"]{Racket version 8.0} is available.}
  ◊div[#:style "flex-grow:1"]{}
   ◊div[#:style "margin-right: 1em"]{◊link["books.html"]{We welcome two new Racket books:}
                          (1) Racket Programming the Fun Way
