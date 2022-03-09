c1aa27fe9e15e4b00f9914c44876e314788309da
morazanm <morazanm@gmail.com>
2022-03-09 10:54:32 -0500

    Update books.html.pm
    Added Animated Problem Solving

--- www/books.html.pm
+++ www/books.html.pm
@@ -16,6 +16,9 @@ Learn to design programs systematically, with pictures.
 ◊link["https://www.realmofracket.com/"]{Realm of Racket}
 Learn to program with Racket, one game at a time.
 
+◊link["https://rd.springer.com/book/10.1007/978-3-030-85091-3"]{Animated Problem Solving}
+Learn to design programs systematically using video games.
+
 ◊link["https://gustavus.edu/mcs/max/concrete-abstractions.html"]{Concrete Abstractions}
 An introduction to computer science using Scheme.
 
