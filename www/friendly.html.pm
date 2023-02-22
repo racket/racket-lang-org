320870f180c3cd41698145e60afded0ede83afb2
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2023-02-22 13:22:04 +0000

    Update to add community moderation team

--- www/friendly.html.pm
+++ www/friendly.html.pm
@@ -11,7 +11,20 @@ The Racket community aims to improve the world through programming. It started w
 
 Racket project management wants to encourage and preserve this open exchange of ideas, which requires an environment that enables all to participate without fear of personal harassment. We define ◊em{harassment} to include specific unacceptable factors and behaviors listed in ◊link["https://www.acm.org/about-acm/policy-against-harassment"]{the ACM's policy against harassment}. Unacceptable behavior will not be tolerated.
 
-Anyone witnessing or subject to unacceptable behavior should notify ◊link["team.html#management"]{Racket project management}, which can be reached at ◊link["mailto:management@racket-lang.org"]{management@racket-lang.org}.
+Anyone witnessing or subject to unacceptable behaviour should notify the moderation team or Racket Project management.
+
+Community moderation team:
+
+◊link["https://github.com/countvajhula"]{Siddhartha Kasivajhula}
+
+◊link["https://github.com/jackfirth"]{Jack Firth}
+
+◊link["https://github.com/samdphillips"]{Sam Phillips}
+
+◊link["https://github.com/spdegabrielle"]{Stephen De Gabrielle}
+
+
+◊link["team.html#management"]{Racket project management} can be reached at ◊link["mailto:management@racket-lang.org"]{management@racket-lang.org}.
 
 ◊p[#:id "bottom"]{The wording of this policy is directly derived from ◊link["http://snapl.org/2015/policy.html"]{that of the SNAPL conference}, which is in turn derived from that of the ACM, with thanks to both.}
 }
