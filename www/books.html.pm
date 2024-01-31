2880fc36d73e358ce1c6606c049a0bafd542c286
Matthias Felleisen <matthias@ccs.neu.edu>
2024-01-31 07:56:54 -0500

    2 sites for Realm

--- www/books.html.pm
+++ www/books.html.pm
@@ -14,7 +14,7 @@ Learn to design programs systematically.
 Learn to design programs systematically, with pictures.
 
 ◊link["https://nostarch.com/realmofracket.htm"]{Realm of Racket}
-Learn to program with Racket, one game at a time.
+Learn to program with Racket, one game at a time; see also ◊link["https://www.realmofracket.com/"]{its companion site}
 
 ◊link["https://rd.springer.com/book/10.1007/978-3-030-85091-3"]{Animated Problem Solving}
 Learn to design programs systematically using video games.
