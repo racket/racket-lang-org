ebf98f409a164366a4554803eaf34ea3aef3a46a
Jesse Alama <jesse@serverracket.com>
2024-07-30 10:36:36 +0200

    rcon 2024: Add link to Ben Knoble's frosthaven-manager

--- rcon/2024/index.rkt
+++ rcon/2024/index.rkt
@@ -383,11 +383,7 @@ Racket’s macros are fantastic for building DSLs that mix well with general-pur
 @talk{Frosthaven Manager: Built by the Community}
 #:more
 @abstract{
-Consisting of more than 15k lines of code and documentation,
-written in spare time during the last two and a half years, the
-Frosthaven Manager would not exist without the Racket community. Let’s
-explore some of the community’s contributions, reflect on the benefits
-of building in the open, and dream about what comes next.
+Consisting of more than 15k lines of code and documentation, written in spare time during the last two and a half years, the @(a #:href "https://github.com/benknoble/frosthaven-manager" #:title "frosthaven-manager (GitHub)" "Frosthaven Manager") would not exist without the Racket community. Let’s explore some of the community’s contributions, reflect on the benefits of building in the open, and dream about what comes next.
 }
 ]
 
