c128ceb313c8c4cdebb2669e6fd9ec116379cfaf
Matthew Flatt <mflatt@racket-lang.org>
2025-11-23 10:35:37 -0700

    fix spelling

--- www/sfc.html.pm
+++ www/sfc.html.pm
@@ -4,7 +4,7 @@
 
 ◊(top)
 
-◊p[#:class "one-column-body-text" #:id "pull-quote"]{From June 2018 until Ocotober 2025, Racket ◊link["https://sfconservancy.org/news/2018/jun/12/racketjoins/"]{was a member} of ◊link["https://sfconservancy.org/"]{Software Freedom Conservancy}, a nonprofit organization that promotes open-source software. 
+◊p[#:class "one-column-body-text" #:id "pull-quote"]{From June 2018 until October 2025, Racket ◊link["https://sfconservancy.org/news/2018/jun/12/racketjoins/"]{was a member} of ◊link["https://sfconservancy.org/"]{Software Freedom Conservancy}, a nonprofit organization that promotes open-source software. 
 }
 
 ◊p{Racket now has its own ◊link["rplf.html"]{Racket Programming Language Foundation}. See its ◊link["rplf.html"]{page} for more information.}
