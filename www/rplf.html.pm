e87cb27b7cd0b17f14704996776c0b688384ff9f
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2026-05-17 22:38:30 +0100

    update rplf

--- www/rplf.html.pm
+++ www/rplf.html.pm
@@ -6,40 +6,42 @@
 
 ◊special-section[#:class "one-column-body-text" #:id "pull-quote"]{The ◊b{Racket Programming Language Foundation} supports Racket development.}
 
-The Racket Programming Language Foundation is registered in Delaware and is recognized as a 501(3)(c) public charity in the US.
+◊section[#:class "one-column-body-text"]{
+◊div{About}
 
-Donations, both in-kind and monetary, are used for hosting community infrastructure, administration, educational outreach, and community events such as RacketCon and Racket School.
+◊p{The Racket Programming Language Foundation is registered in Delaware and is recognized as a 501(3)(c) public charity in the US.}
 
-Thank you for supporting Racket.
+◊p{Donations, both in-kind and monetary, are used for hosting community infrastructure, administration, educational outreach, and community events such as RacketCon and Racket School.}
+
+◊p{Thank you for supporting Racket.}
+}
+◊section[#:class "one-column-body-text"]{
+◊div{Donate}
+
+◊p{You can ◊link["https://github.com/sponsors/Racket"]{sponsor Racket} via GitHub.}
+
+◊p{Donations are usually tax-deductible in the US.}
+
+◊p{Many companies offer a matching gift program to encourage philanthropy. You may be able to increase the impact of your donations and your employer may receive a tax deduction for contributions to the Racket Programming Language Foundation (EIN 04-3594598), which is a public charity classified as exempt under section 501(c)(3) of the Internal Revenue Code. Please contact your our company's Human Resources or Accounting Department for details of requirements or forms. Please contact us at ◊link["mailto:management@racket-lang.org"]{management@racket-lang.org} with any questions.}
+}
 
 ◊section[#:class "one-column-body-text"]{
-◊div{Donate
+◊div{Sponsorship}
+◊p{Please contact us at ◊link["mailto:management@racket-lang.org"]{management@racket-lang.org} to discuss sponsorship oppotunities.}
 
-You can ◊link["https://github.com/sponsors/Racket"]{sponsor Racket} via GitHub.
-
-Donations are usually tax-deductible in the US.
-
-Many companies offer a matching gift program to encourage philanthropy. You may be able to increase the impact of your donations and your employer may receive a tax deduction for contributions to the Racket Programming Language Foundation (EIN 04-3594598), which is a public charity classified as exempt under section 501(c)(3) of the Internal Revenue Code. Please contact your our company's Human Resources or Accounting Department for details of requirements or forms. Please contact us at ◊link["mailto:management@racket-lang.org"]{management@racket-lang.org} with any questions.
-}}
+◊p{The Racket Programming Language Foundation is recognized by the IRS as a 501(c)(3) public charity in the US, and donations are tax-deductible for organizations that pay taxes in the United States.}
+}
 
 ◊section[#:class "one-column-body-text"]{
-◊div{Sponsorship
+◊div{Other ways to give}
 
-Please contact us at ◊link["mailto:management@racket-lang.org"]{management@racket-lang.org} to discuss sponsorship oppotunities.
+◊p{We also accept checks drawn in US dollars from US banks. Make your check payable to "Racket Programming Language Foundation" and mail to:}
 
-The Racket Programming Language Foundation is recognized by the IRS as a 501(c)(3) public charity in the US, and donations are tax-deductible for organizations that pay taxes in the United States.
-}}
-
-◊section[#:class "one-column-body-text"]{
-◊div{Other ways to give
-
-We also accept checks drawn in US dollars from US banks. Make your check payable to "Racket Programming Language Foundation" and mail to:
-
-Racket Programming Language Foundation
+◊pre{Racket Programming Language Foundation
 2285 S Benchmark Cir
-Salt Lake City, UT 84109
+Salt Lake City, UT 84109}
 
-EIN: 33-4854933
+◊p{EIN: 33-4854933}
 
-Thank you for supporting Racket!
-}}
+◊p{Thank you for supporting Racket!}
+}
