c97e84d82f01923672a6247fc1fc8077094ebc85
John Clements <clements@racket-lang.org>
2026-05-27 22:32:15 -0700

    update front page link

--- www/index.html.pm
+++ www/index.html.pm
@@ -43,7 +43,7 @@
 ◊special-section[#:class "one-column-body-text" #:style "font-size:77%" #:id "pull-quote"]{
 ◊div[#:class "container-fluid vertical-in-smartphones-horizontal-otherwise"]{
  ◊div[#:style "margin-left: 1em"]{
-   ◊link["https://blog.racket-lang.org/2026/02/racket-v9-1.html"]{Racket version 9.1} is available.}
+   ◊link["https://blog.racket-lang.org/2026/05/racket-v9-2.html"]{Racket version 9.2} is available.}
 
  ◊div[#:style "flex-grow:1"]{}
 
