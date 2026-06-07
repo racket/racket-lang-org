331d5cc92906c1cd641e2d0c72431062959ab000
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2026-06-07 11:10:38 +0100

    Update rplf.html.pm tax (#374)
    * Update rplf.html.pm tax
    
    added 
    > As a 501(3)(c) public charity in the US, donations are fully tax-deductible to the extent permitted by law.
    
    * Update rplf.html.pm
    
    recognizes - recognized
    
    * Update rplf.html.pm
    
    * update rplf
    
    * rplf.html upate
    
    * RPLF fix EIN
    
    * remove "rplf.html"
    
    ---------
    
    Co-authored-by: Matthew Flatt <mflatt@racket-lang.org>

--- www/rplf.html.pm
+++ www/rplf.html.pm
@@ -7,23 +7,41 @@
 ◊special-section[#:class "one-column-body-text" #:id "pull-quote"]{The ◊b{Racket Programming Language Foundation} supports Racket development.}
 
 ◊section[#:class "one-column-body-text"]{
+◊div{About}
 
+◊p{The Racket Programming Language Foundation is registered in Delaware and is recognized as a 501(3)(c) public charity in the US.}
+
+◊p{Donations, both in-kind and monetary, are used for hosting community infrastructure, administration, educational outreach, and community events such as RacketCon and Racket School.}
+
+◊p{Thank you for supporting Racket.}
+}
+◊section[#:class "one-column-body-text"]{
 ◊div{Donate}
 
-◊p{
-The Racket Programming Language Foundation is registered in Delaware and is recognizes as a 501(3)(c) public charity in the US.
+◊p{You can ◊link["https://github.com/sponsors/Racket"]{sponsor Racket} via GitHub.}
 
-Donations, both in-kind and monetary, are used for hosting community infrastructure, administration, educational outreach, and community events such as RacketCon and Racket School.}
+◊p{Donations are usually tax-deductible in the US.}
 
-You can ◊link["https://github.com/sponsors/Racket"]{sponsor Racket} via GitHub.
+◊p{Many companies offer a matching gift program to encourage philanthropy. You may be able to increase the impact of your donations and your employer may receive a tax deduction for contributions to the Racket Programming Language Foundation (EIN 33-4854933), which is a public charity classified as exempt under section 501(c)(3) of the Internal Revenue Code. Please contact your our company's Human Resources or Accounting Department for details of requirements or forms. Please contact us at ◊link["mailto:management@racket-lang.org"]{management@racket-lang.org} with any questions.}
+}
 
-We also accept checks drawn in US dollars from US banks. Make your check payable to "Racket Programming Language Foundation" and mail to:
+◊section[#:class "one-column-body-text"]{
+◊div{Sponsorship}
+◊p{Please contact us at ◊link["mailto:management@racket-lang.org"]{management@racket-lang.org} to discuss sponsorship oppotunities.}
 
-Racket Programming Language Foundation
+◊p{The Racket Programming Language Foundation is recognized by the IRS as a 501(c)(3) public charity in the US, and donations are tax-deductible for organizations that pay taxes in the United States.}
+}
+
+◊section[#:class "one-column-body-text"]{
+◊div{Other ways to give}
+
+◊p{We also accept checks drawn in US dollars from US banks. Make your check payable to "Racket Programming Language Foundation" and mail to:}
+
+◊pre{Racket Programming Language Foundation
 2285 S Benchmark Cir
-Salt Lake City, UT 84109
+Salt Lake City, UT 84109}
 
-EIN: 33-4854933
+◊p{EIN: 33-4854933}
 
-Thank you for supporting Racket!
+◊p{Thank you for supporting Racket!}
 }
